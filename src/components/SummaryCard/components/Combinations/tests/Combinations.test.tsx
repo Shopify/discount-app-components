0233737b8534aee215cbac040af119c81cbba703
bkspace <sammyisseyegh@gmail.com>
2023-08-21 15:15:57 +0100

    Include "no combinations" in combinations && include selection in test name

--- src/components/SummaryCard/components/Combinations/tests/Combinations.test.tsx
+++ src/components/SummaryCard/components/Combinations/tests/Combinations.test.tsx
@@ -3,22 +3,9 @@ import {mountWithApp} from 'tests/utilities';
 import {Combinations} from '../Combinations';
 
 describe('<Combinations />', () => {
-  it('renders nothing when no combination options are set', () => {
-    const combinations = mountWithApp(
-      <Combinations
-        combinesWith={{
-          orderDiscounts: false,
-          productDiscounts: false,
-          shippingDiscounts: false,
-        }}
-      />,
-    );
-
-    expect(combinations.find(Combinations)).toBeNull();
-  });
-
   it.each`
     combinesWithProduct | combinesWithOrder | combinesWithShipping | expectedText
+    ${false}            | ${false}          | ${false}             | ${`Can't combine with other discounts`}
     ${true}             | ${true}           | ${true}              | ${`Combines with product, order, and shipping discounts`}
     ${true}             | ${true}           | ${false}             | ${`Combines with product and order discounts`}
     ${true}             | ${false}          | ${true}              | ${`Combines with product and shipping discounts`}
@@ -27,7 +14,7 @@ describe('<Combinations />', () => {
     ${false}            | ${true}           | ${false}             | ${`Combines with order discounts`}
     ${false}            | ${false}          | ${true}              | ${`Combines with shipping discounts`}
   `(
-    'renders combination summary when at least one combination option is set',
+    'renders the summary when combinesWith `product: $combinesWithProduct, order: $combinesWithOrder, shipping: $combinesWithShipping`',
     ({
       combinesWithProduct,
       combinesWithOrder,
